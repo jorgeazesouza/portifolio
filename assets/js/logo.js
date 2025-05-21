3daf91741d54f1c4ae1f134b168447b5071eb397
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-05-21 11:12:55 -0300

    quase finalizado

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -2,6 +2,8 @@ const html = document.querySelector('.html');
 const css = document.querySelector('.css');
 const javascript = document.querySelector('.javascript');
 const python = document.querySelector('.python');
+const git = document.querySelector('.git');
+const github = document.querySelector('.github');
 const text = document.querySelector('#hab-text');
 
 html.addEventListener('mouseenter', function() {
@@ -52,5 +54,29 @@ python.addEventListener('mouseenter', function() {
 
 python.addEventListener('mouseleave', function() {
     python.src = 'img/logos-habilidades/python-logo.svg';
+    text.innerHTML = 'Passe o mouse por cima para saber mais';
+});
+
+git.addEventListener('mouseenter', function() {
+    git.src = 'img/logos-habilidades/git-cor.svg';
+    text.innerHTML = '<h3>Git</h3><br>';
+    text.innerHTML += '<p>Git é um sistema de controle de versão usado para acompanhar mudanças no código-fonte durante o desenvolvimento de software.</p><br>';
+    text.innerHTML += '<p>Tenho conhecimentos básicos no uso do Git, como iniciar repositórios, realizar commits, criar branches e acompanhar o histórico de alterações.</p>';
+});
+
+git.addEventListener('mouseleave', function() {
+    git.src = 'img/logos-habilidades/git-logo.svg';
+    text.innerHTML = 'Passe o mouse por cima para saber mais';
+});
+
+github.addEventListener('mouseenter', function() {
+    github.src = 'img/logos-habilidades/github-cor.svg';
+    text.innerHTML = '<h3>GitHub</h3><br>';
+    text.innerHTML += '<p>GitHub é uma plataforma online que permite armazenar e gerenciar repositórios Git na nuvem, facilitando o trabalho colaborativo entre desenvolvedores.</p><br>';
+    text.innerHTML += '<p>Tenho familiaridade com os principais recursos, como clonar projetos, subir arquivos, e colaborar por meio de pull requests.</p>'
+});
+
+github.addEventListener('mouseleave', function() {
+    github.src = 'img/logos-habilidades/github-logo.svg';
     text.innerHTML = 'Passe o mouse por cima para saber mais';
 });
